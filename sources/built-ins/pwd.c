4c0ae9ee79388cad4cec33e7c9800db147d6089c
Adrià Márquez Nebot <adrmarqu@car9s6.42barcelona.com>
2024-06-24 14:41:45 +0200

    Mensaje de error de pwd

--- sources/built-ins/pwd.c
+++ sources/built-ins/pwd.c
@@ -6,7 +6,7 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 16:21:35 by adrmarqu          #+#    #+#             */
-/*   Updated: 2024/06/16 11:51:51 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2024/06/24 14:41:26 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	print_pwd(void)
 	cwd = getcwd(NULL, 0);
 	if (!cwd)
 	{
-		perror("bash: pwd");
+		fd_printf(1, "pwd: %s", strerror(errno));
 		return (1);
 	}
 	printf("%s\n", cwd);
