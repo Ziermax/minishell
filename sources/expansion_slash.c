458c5f1f70834ab4579c42f053937e1fea8164f5
Adrià Márquez Nebot <adrmarqu@car11s1.42barcelona.com>
2024-08-12 16:03:58 +0200

    Acabado el add/rmv del slash

--- sources/expansion_slash.c
+++ sources/expansion_slash.c
@@ -6,84 +6,53 @@
 /*   By: adrmarqu <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/03 13:42:58 by adrmarqu          #+#    #+#             */
-/*   Updated: 2024/08/12 13:23:09 by adrmarqu         ###   ########.fr       */
+/*   Updated: 2024/08/12 16:03:15 by adrmarqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../Libft/includes/libft.h"
 #include "../includes/expand.h"
-/*
-static int	slash_remove_len(char *str, char *keep)
-{
-	int	i;
-	int	len;
 
-	i = 0;
-	len = 0;
-	while (str[i])
+static char	*remove_process(char *str, void *keep, char *normalize, int *i)
+{
+	if (*str == '\\' && !*(str + 1))
+		str++;
+	else if (*str == '\\' && ft_strchr(keep, *(str + 1)))
 	{
-		if (str[i] == '\\')
-			i++;
-		if (!str[i])
-			break ;
-		if (ft_strchr(keep, str[i]))
-			len++;
-		len++;
-		i++;
+		normalize[(*i)++] = *(str++);
+		normalize[(*i)++] = *(str++);
 	}
-	return (len);
+	else if (*str == '\\' && !ft_strchr(keep, *(str + 1)))
+	{
+		str++;
+		normalize[(*i)++] = *(str++);
+	}
+	else
+		normalize[(*i)++] = *(str++);
+	return (str);
 }
 
-static int	slash_add_len(char *str, char *keep)
-{
-	int	len;
-	int	i;
-
-	len = 0;
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] == '\\' || ft_strchr(keep, str[i]))
-		{
-			i += 2;
-			len += 2;
-		}
-		else
-		{
-			i++;
-			len++;
-		}
-	}
-	return (len);
-}
-
-char	*remove_slash(char *str, char *keep)
+char	*remove_slash(char *str, void *keep)
 {
 	char	*normalize;
+	char	*ret_normalize;
 	int		i;
-	int		len;
 
 	if (!str || !keep)
 		return (NULL);
-	len = slash_remove_len(str, keep);
-	normalize = malloc((len + 1) * sizeof(char *));
+	normalize = malloc((ft_strlen(str) + 1) * sizeof(char));
 	if (!normalize)
 		return (NULL);
 	i = 0;
-	while (*str && i < len)
-	{
-		if (*str == '\\')
-			str++;
-		if (!*str)
-			break ;
-		if (i < len)
-			normalize[i++] = *(str++);
-	}
+	while (*str)
+		str = remove_process(str, keep, normalize, &i);
 	normalize[i] = '\0';
-	return (normalize);
-}*/
+	ret_normalize = ft_strdup(normalize);
+	free(normalize);
+	return (ret_normalize);
+}
 
-static char	*add_process(char *str, char *keep, char *slashed, int *i)
+static char	*add_process(char *str, void *keep, char *slashed, int *i)
 {
 	if (*str == '\\' && !*(str + 1))
 		str++;
@@ -92,15 +61,10 @@ static char	*add_process(char *str, char *keep, char *slashed, int *i)
 		slashed[(*i)++] = *(str++);
 		slashed[(*i)++] = *(str++);
 	}
-	else if (*str == '\\' && !ft_strchr(keep, *(str + 1)))
-	{
-		str++;
-		slashed[(*i)++] = *(str++);
-	}
 	else if (*str == '\\' && *(str + 1) == '\\')
 	{
-		slashed[(*i)++] = '\\';
-		str += 2;
+		slashed[(*i)++] = *(str++);
+		slashed[(*i)++] = *(str++);
 	}
 	else if (ft_strchr(keep, *str))
 	{
@@ -112,37 +76,41 @@ static char	*add_process(char *str, char *keep, char *slashed, int *i)
 	return (str);
 }
 
-char	*add_slash(char *str, char *keep)
+char	*add_slash(char *str, void *keep)
 {
 	char	*slashed;
-	char	*m_slashed;
+	char	*ret_slashed;
 	int		i;
 
 	if (!str || !keep)
 		return (NULL);
-	slashed = malloc(ft_strlen(str) * 2 * sizeof(char));
+	slashed = malloc((ft_strlen(str) * 2 + 1) * sizeof(char));
 	if (!slashed)
 		return (NULL);
 	i = 0;
 	while (*str)
 		str = add_process(str, keep, slashed, &i);
 	slashed[i] = '\0';
-	m_slashed = ft_strdup(slashed);
+	ret_slashed = ft_strdup(slashed);
 	free(slashed);
-	return (m_slashed);
+	return (ret_slashed);
 }
-
-/*#include <stdio.h>
+/*
+#include <stdio.h>
 
 int	main(int ac, char **av)
 {
 	char *str1;
+	char *str2;
 
-	if (ac != 3)
+	if (ac != 4)
 		return (1);
 	printf("BFR: %s\n", av[1]);
 	str1 = add_slash(av[1], av[2]);
 	printf("ADD: %s\n", str1);
+	str2 = remove_slash(str1, av[3]);
+	printf("RMV: %s\n", str2);
 	free(str1);
+	free(str2);
 	return (0);
 }*/
